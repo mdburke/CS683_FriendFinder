b2a04d3365d10093bcf35e3a97643766d3f30f3d
Michael Burke <mike.d.burke@gmail.com>
2017-09-06 21:53:51 -0400

    Add saving of state based on the last button clicked.

--- app/src/main/java/com/imminentapps/friendfinder/HomeScreen.java
+++ app/src/main/java/com/imminentapps/friendfinder/HomeScreen.java
@@ -1,22 +1,20 @@
 package com.imminentapps.friendfinder;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class HomeScreen extends AppCompatActivity {
     private static final String DEFAULT_TAG = "DefaultTag";
+    private TextView outputTextView;
 
     /******** Lifecycle Methods *********/
 
@@ -26,6 +24,7 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_home_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        outputTextView = (TextView) findViewById(R.id.outputText);
         initializeButtonListeners();
     }
 
@@ -51,23 +50,44 @@ public class HomeScreen extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        outputTextView.setText(savedInstanceState.getCharSequence("OutputText"));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putCharSequence("OutputText", outputTextView.getText());
+    }
+
     /***** Private Helper Methods *******/
+
+
+    @SuppressLint("SetTextI18n")
     private void initializeButtonListeners() {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener((view) -> Snackbar
                 .make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
 
-        TextView searchForFriendsButton = (TextView) findViewById(R.id.buttonSearchForFriends);
-        searchForFriendsButton.setOnClickListener((view) ->
-                Log.i(DEFAULT_TAG, "Go to SearchForFriends page here."));
+        Button searchForFriendsButton = (Button) findViewById(R.id.buttonSearchForFriends);
+        searchForFriendsButton.setOnClickListener((view) -> {
+            Log.i(DEFAULT_TAG, "Go to SearchForFriends page here.");
+            outputTextView.setText("SearchForFriends last clicked.");
+        });
 
-        TextView editProfileButton = (TextView) findViewById(R.id.buttonEditProfile);
-        editProfileButton.setOnClickListener((view) ->
-                Log.i(DEFAULT_TAG, "Go to EditProfile page here."));
+        Button editProfileButton = (Button) findViewById(R.id.buttonEditProfile);
+        editProfileButton.setOnClickListener((view) -> {
+            Log.i(DEFAULT_TAG, "Go to EditProfile page here.");
+            outputTextView.setText("EditProfile last clicked.");
+        });
 
-        TextView editAccountSettingsButton = (TextView) findViewById(R.id.buttonEditAccountSettings);
-        editAccountSettingsButton.setOnClickListener((view) ->
-                Log.i(DEFAULT_TAG, "Go to EditAccountSettings page here."));
+        Button editAccountSettingsButton = (Button) findViewById(R.id.buttonEditAccountSettings);
+        editAccountSettingsButton.setOnClickListener((view) -> {
+            Log.i(DEFAULT_TAG, "Go to EditAccountSettings page here.");
+            outputTextView.setText("EditAccountSettings last clicked.");
+        });
     }
 }
