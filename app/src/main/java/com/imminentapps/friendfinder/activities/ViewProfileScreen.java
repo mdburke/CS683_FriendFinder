964504384abce6daf751474c3956fb7d94dbc0ad
Michael Burke <mike.d.burke@gmail.com>
2017-09-13 22:02:18 -0400

    Add more mock user profile data

--- app/src/main/java/com/imminentapps/friendfinder/activities/ViewProfileScreen.java
+++ app/src/main/java/com/imminentapps/friendfinder/activities/ViewProfileScreen.java
@@ -3,16 +3,22 @@ package com.imminentapps.friendfinder.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.imminentapps.friendfinder.R;
+import com.imminentapps.friendfinder.domain.Profile;
 import com.imminentapps.friendfinder.domain.User;
 
 public class ViewProfileScreen extends AppCompatActivity {
     private User currentUser;
+    private Profile currentProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile_screen);
 
         Intent intent = getIntent();
 
@@ -22,6 +28,16 @@ public class ViewProfileScreen extends AppCompatActivity {
             throw new IllegalStateException("Activity was not passed a valid user object.");
         }
 
-        setContentView(R.layout.activity_view_profile_screen);
+        currentProfile = currentUser.getProfile();
+
+        TextView usernameView = (TextView) findViewById(R.id.usernameTextView);
+        ListView listView = (ListView) findViewById(R.id.hobbyListView);
+        TextView aboutMeView = (TextView) findViewById(R.id.aboutMeTextView);
+
+        usernameView.setText(currentProfile.getUsername());
+        aboutMeView.setText(currentProfile.getAboutMeSection());
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, currentProfile.getHobbies());
+        listView.setAdapter(adapter);
     }
 }
