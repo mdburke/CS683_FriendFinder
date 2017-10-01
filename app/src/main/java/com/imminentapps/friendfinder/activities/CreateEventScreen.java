a9883126fd8ae0d3a1ce0ca55843050e56f0683c
Michael Burke <mike.d.burke@gmail.com>
2017-09-30 22:30:03 -0400

    Get DatePicker working

--- app/src/main/java/com/imminentapps/friendfinder/activities/CreateEventScreen.java
+++ app/src/main/java/com/imminentapps/friendfinder/activities/CreateEventScreen.java
@@ -1,15 +1,40 @@
 package com.imminentapps.friendfinder.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import com.imminentapps.friendfinder.R;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class CreateEventScreen extends AppCompatActivity {
+    private TextView eventTitle;
+    private TextView eventLocation;
+    private DatePicker eventDate;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event_screen);
+
+        // Initialize views
+        eventTitle = findViewById(R.id.eventName_EditText);
+        eventLocation = findViewById(R.id.eventLocation_EditText);
+        eventDate = findViewById(R.id.eventDatePicker);
+        saveButton = findViewById(R.id.createEvent_Savebutton);
+        saveButton.setOnClickListener(view -> saveButtonClicked());
+    }
+
+    private void saveButtonClicked() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(eventDate.getYear(), eventDate.getMonth(), eventDate.getDayOfMonth());
+        Date date = calendar.getTime();
+        Log.i("tag", date.toString());
     }
 }
---
+++ app/src/main/java/com/imminentapps/friendfinder/domain/Event.java
@@ -0,0 +1,8 @@
+package com.imminentapps.friendfinder.domain;
+
+/**
+ * Created by mburke on 9/30/17.
+ */
+
+public class Event {
+}
